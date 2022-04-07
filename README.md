bf71dac2acb23135caa974168456ef314666810e
aledcuevas <aledancuevas@gmail.com>
2022-04-07 14:34:19 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# dread-scraper
-A quick and dirty scraper for Dread.
+## Lightweight Selenium Scraper for Dread
+
+The following is a brief introduction on how to use this code to scrape Dread.
+
+### Requirements
+
+You will find the necessary libraries and imports at the top of `main.py`.
+I'm also using Tor Browser `v.11.0.9` (based on Mozilla Firefox `91.7.0esr`). I'm
+running Selenium `4.1.3`.
+
+### High-level Flow
+
+The code has two routines to retrieve lists of URLs, and one to retrieve the
+contents inside the URLs. To retrieve lists of URLs, we can either start by
+navigating the pages from the homepage of Dread, or we can execute a search and
+navigate the search results' pages. Thus, if desired, this process can be split
+across two browsers, one to retrieve URLs, one to navigate through those URLs.
+
+### Directory Structure
+
+Following is a sample tree view for how your directory should look.
+```
+./dread-scraper/
+    main.py
+    ./data/
+        ./search_term=scam/
+            ./scrapes/
+                ./<post_id_1>/
+                    ./post.html
+                .   
+                .
+                ./<post_id_n>/
+            urls.txt
+            PAGE
+```
+### Procedure
+
+This scraper requires manual setup to get it to begin scraping, due to issues
+with the Tor Browser software, as well as having to solve Dread's CAPTCHA. To do
+this, I conduct the following procedure:
+
+- First, terminate all Tor Browser instances you may have running. On my end, 
+there seems to be a bug where Tor Browser will not connect to the Tor network
+if launched by Selenium when there's another instance active.
+- Once the Tor Browser is launched, wait for it to connect to the circuits. From
+here, you can try navigating to a website as a test. I use `google.com`
+- If working properly, navigate to Dread's main page with the `navigate_to_main()`
+function. Here you will need to wait an arbitrary amount of time determined by Dread (typically,
+between 1-3 mins.). After that, you will be redirected to a CAPTCHA which you need to solve
+in under 1 min.
+- Once you are on the mainpage of Dread you can begin retrieving links. You can, for example,
+launch `get_post_links_through_main('1')` to begin retrieving all posts' links from each page,
+starting from page 1. Note that, the main page of Dread indexes posts from various sub-Dreads.
+This process usually works flawlessly for a while. At some point, though, Dread seems to ask
+again for a CAPTCHA although I'm not entirely sure what triggers that yet.
+- Once satisfied with the number of URLs that have been retrieved (or we have hit 500,
+which is the number of pages that Dread indexes), we can proceed to navigate each URL and
+save the source, using `get_source_pages_from_list('/path/to/search_term=scam/scrapes', '/path/to/search_term=scam/urls.txt')`
+(example based on the above file structure). If everything is working properly, the `scrapes`
+folder should start getting populated with folders named after the post IDs.
---
+++ main.py
@@ -0,0 +1,202 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from selenium import webdriver
+#from selenium.webdriver.firefox.firefox_binary import
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.common.keys import Keys
+import json
+from datetime import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import random
+import codecs
+import os
+
+
+import pandas as pd
+
+DATA_PATH = '/Users/acuevasv/Dev/dread-scraper/data/'
+
+# Links of interest
+dread_main = 'http://dreadytofatroptsdj6io7l3xptbet6onoyno2yv7jicoxknyazubrad.onion'
+dread_main_w_pagination = 'http://dreadytofatroptsdj6io7l3xptbet6onoyno2yv7jicoxknyazubrad.onion/?p={}'.format('PAGE')
+
+dread_search = 'http://dreadytofatroptsdj6io7l3xptbet6onoyno2yv7jicoxknyazubrad.onion/search/?q={}&sort=best&fuzziness=auto'.format('SEARCH_TERM')
+dread_search_w_pagination = 'http://dreadytofatroptsdj6io7l3xptbet6onoyno2yv7jicoxknyazubrad.onion/search/?p={}&sort=best&q={}&fuzziness=auto'.format('PAGE', 'SEARCH_TERM')
+
+
+def get_post_links_through_search(search_term, start_page):
+    # E.g., search_term = 'scammer'
+    #       start_page = '1'
+    print('{} - Begin saving links from Dread search.'.format(datetime.now().strftime("%H:%M:%S")))
+    print('SEARCH TERM: {}'.format(search_term))
+    print('STARTING PAGE: {}'.format(start_page))
+
+    # Create the folder where we are going to save the scrapes
+    scrapes_savepath = DATA_PATH + 'search_term={}/'.format(search_term)
+    if not os.path.isdir(scrapes_savepath):
+        os.mkdir(scrapes_savepath)
+    else:
+        print('Error: We may have already scraped this search term, double check save path...')
+        return -1
+
+    print('Saving results to {}'.format(scrapes_savepath))
+
+    url_list = []
+    current_page = int(start_page)
+    page_ct =0
+    ##### SCRAPE SEARCH RESULTS AND GET URLs
+    while True:
+        now = datetime.now()
+        page_ct += 1
+        if random.randint(5,12) == page_ct:
+            print('Breaking within pages to not get rate limited.')
+            page_ct = 0
+            time.sleep(random.uniform(30,120))
+
+        dread_search_w_pagination = 'http://dreadytofatroptsdj6io7l3xptbet6onoyno2yv7jicoxknyazubrad.onion/search/?p={}&sort=best&q={}&fuzziness=auto'\
+        .format(current_page, search_term)
+        print('{} - Loading PAGE {}....'.format(now.strftime("%H:%M:%S"), current_page))
+
+        driver.get(dread_search_w_pagination)
+        elements = driver.find_elements_by_class_name("title")
+        for e in elements:
+            try:
+                post_url = e.get_attribute('href')
+                url_list.append(post_url)
+                print('Saved: {}'.format(post_url))
+            except Exception as e:
+                print(e)
+
+        print('Saving progress to {}/urls-500.txt'.format(scrapes_savepath))
+        with open(scrapes_savepath + 'urls-500.txt'.format(search_term), 'w') as fw:
+            json.dump(url_list, fw)
+
+        with open(scrapes_savepath + 'PAGE', 'w') as fw:
+            json.dump(current_page, fw)
+
+        current_page = current_page + 1
+
+        if current_page == 499:
+            break
+        print('{} - Finished page {}, continuing to page {}'.format(now.strftime("%H:%M:%S"), current_page, current_page + 1))
+        time.sleep(random.uniform(9,15))
+
+    return url_list
+
+
+def get_post_links_through_main(start_page):
+    # E.g., start_page = '1'
+    print('{} - Begin saving links from Dread Homepage.'.format(datetime.now().strftime("%H:%M:%S")))
+    '''
+    Create the folder where we are going to save the scrapes
+    Because we are scraping based on the main page, we save the time date when we started scraping instead
+    As time passes, the content of each page will change
+    '''
+    scrapes_savepath = DATA_PATH + 'homepage_scrape_date={}/'.format(datetime.now().strftime("%d_%m_%y"))
+    if not os.path.isdir(scrapes_savepath):
+        os.mkdir(scrapes_savepath)
+    else:
+        print('Error: We may have already started scraping today, double check the contents in the savepath...')
+        return -1
+
+    url_list = []
+    current_page = int(start_page)
+    page_ct = 0
+    ##### SCRAPE PAGES AND GET URLs
+    while True:
+     now = datetime.now()
+     page_ct += 1
+     # We add a random sleep interval every couple of pages to prevent getting rate limited.
+     if random.randint(5,12) % page_ct == 0:
+         print('Breaking within pages to not get rate limited.')
+         page_ct = 0
+         time.sleep(random.uniform(30,120))
+
+     dread_main_w_pagination = 'http://dreadytofatroptsdj6io7l3xptbet6onoyno2yv7jicoxknyazubrad.onion/?p={}'\
+     .format(current_page)
+     print('{} - Loading PAGE {}....'.format(now.strftime("%H:%M:%S"), current_page))
+
+     driver.get(dread_main_w_pagination)
+     elements = driver.find_elements_by_class_name("title")
+     for e in elements:
+         try:
+             post_url = e.get_attribute('href')
+             url_list.append(post_url)
+             print('Saved: {}'.format(post_url))
+         except Exception as e:
+             print(e)
+
+     print('Saving progress to {}/urls-500.txt'.format(scrapes_savepath))
+     with open(scrapes_savepath + 'urls-500.txt', 'w') as fw:
+         json.dump(url_list, fw)
+
+     with open(scrapes_savepath + 'PAGE', 'w') as fw:
+         json.dump(current_page, fw)
+
+     current_page = current_page + 1
+
+     if current_page == 499:
+         break
+     print('{} - Finished page {}, continuing to page {}'.format(now.strftime("%H:%M:%S"), current_page, current_page + 1))
+     time.sleep(random.uniform(9,15))
+
+    return url_list
+
+def get_source_pages_from_list(save_path, url_list_filepath):
+    #E.g., savepath='/path/to/search_term=scam/scrapes/'
+    #      url_list_filepath = '/path/to/search_term=scam/urls-500.txt
+    print('{} - Going through pages in {} to save source'.format(datetime.now().strftime("%H:%M:%S"), url_list_filepath))
+
+    # Filepath to a list of urls in the following format: ['url_1', 'url_2', ..., 'url_3']
+    # Alternatively, you can pass directly a list of links
+    url_list = None
+    with open(url_list_filepath, 'r') as fr:
+        url_list = json.load(fr)
+
+    for i, url in enumerate(url_list):
+        print('Current index: {}'.format(i))
+        print('{} - Working on: {}'.format(datetime.now().strftime("%H:%M:%S"), url))
+        driver.get(url)
+
+        #Random sleep after we visit a page
+        time.sleep(random.uniform(45,120))
+        post_folder = url.split('/')[-1]
+
+        # If the folder already exists, we assume we have scraped it.
+        if os.path.isdir(save_path + post_folder):
+            continue
+        else:
+        # Else, we create the folder and save the .html
+            os.mkdir(save_path + post_folder)
+            f = codecs.open(save_path + post_folder + '/post.html', 'w', 'utf-8')
+            h = driver.page_source
+            f.write(h)
+            print('Source saved!')
+
+def navigate_to_main():
+    print('{} - Loading MAIN page...'.format(datetime.now().strftime("%H:%M:%S")))
+    driver.get(dread_main)
+
+    print('Saving cookies (if any)...')
+    ch = driver.get_cookies()
+    for c in ch:
+        driver.add_cookie(c)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #binary = FirefoxBinary('/Applications/Tor Browser.app/Contents/MacOS/firefox')
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.binary_location = '/Applications/Tor Browser.app/Contents/MacOS/firefox'
+    driver = webdriver.Firefox(options=firefox_options)
+
+    print('Sleeping for 15 on startup, waiting for TOR connection...')
+    time.sleep(15)
+    driver.get('http://google.com')
+
